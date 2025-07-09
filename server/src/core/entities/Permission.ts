d843c60f1afa35128096955c5f8458b630ef7693
Mr kin <kiengithub@gmail.com>
2025-07-09 16:53:21 +0700

    modify: permission attribute

--- server/src/core/entities/Permission.ts
+++ server/src/core/entities/Permission.ts
@@ -2,13 +2,13 @@
 export type PermissionConstructorParam = {
     id: number
     perName: string
-    description: string | null
+    description?: string
 }
 
 export default class Permission {
     id: number
     perName: string
-    description: string | null
+    description?: string
 
     constructor(options: PermissionConstructorParam) {
         this.id = options.id
