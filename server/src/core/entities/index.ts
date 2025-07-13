b676f581e4096301c8db834995e8137eb83cc65c
Mr kin <kiengithub@gmail.com>
2025-07-13 12:04:04 +0700

    add: implementation RolePerRepository

--- server/src/core/entities/index.ts
+++ server/src/core/entities/index.ts
@@ -1,9 +1,11 @@
 import User, { UserConstructorParam } from "./User.js";
 import Role, { RoleConstructorParam } from "./Role.js";
 import Permission, { PermissionConstructorParam } from "./Permission.js";
+import RolePermission, {RolePermissionParam} from "./RolePermission.js";
 
 export {
     User, UserConstructorParam,
     Role, RoleConstructorParam,
-    Permission, PermissionConstructorParam
+    Permission, PermissionConstructorParam,
+    RolePermission, RolePermissionParam
 }
---
+++ server/src/repositories/RolePerRepository.ts
@@ -0,0 +1,40 @@
+import IRolePerRepository from "core/applications/interfaces/repositories/IRolePerRepository.js";
+import { RolePermission } from "core/entities/index.js"
+import { PrismaClient } from "services/postgresSQL/generated/prisma/client/client";
+
+const prisma = new PrismaClient()
+
+export default class RolePerRepository implements IRolePerRepository {
+    async create(attributes: Pick<RolePermission, "roleId" | "permisId">): Promise<RolePermission> {
+        try {
+            const createdRolePer = await prisma.roles_Permissions.create({
+                data: {
+                    roleId: attributes.roleId,
+                    permisId: attributes.permisId
+                }
+            })
+            return new RolePermission({
+                roleId: createdRolePer.roleId,
+                permisId: createdRolePer.permisId
+            })
+        } catch (error) {
+            throw new Error(`Error: ${error}`)
+        }
+    }
+    async deleteWithPair(attributes: Pick<RolePermission, "roleId" | "permisId">): Promise<boolean> {
+        try {
+            const deletedResult = await prisma.roles_Permissions.delete({
+                where: {
+                    roleId_permisId: {
+                        roleId: attributes.roleId,
+                        permisId: attributes.permisId
+                    }
+                }
+            })
+            return true
+        } catch (error) {
+            console.log(`Error: ${error}`)
+            return false
+        }
+    }
+}
