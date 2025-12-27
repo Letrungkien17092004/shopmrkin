ef46b941ad7be9a80b09001f2de357a2faf1b9c3
Letrungkien17092004 <kiengithub@gmail.com>
2025-12-28 00:46:56 +0700

    config docker

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,5 @@
 import app from "./expressApp/bootstrap.js";
 import { ENV } from "./config/env.js";
-app.listen(ENV.PORT, () => {
-    console.log("listening at: http://localhost:8000")
+app.listen(ENV.PORT || 8000, () => {
+    console.log(`listening at: http://${ENV.SERVER_NAME}:${ENV.PORT}`)
 })
